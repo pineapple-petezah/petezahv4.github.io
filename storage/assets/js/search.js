0dfdb9a306f9c2245abbb5d65c82ebccafd18631
petezahv4 <petezahiscool@gmail.com>
2025-01-01 01:28:51 +0000

    Update search.js

--- storage/assets/js/search.js
+++ storage/assets/js/search.js
@@ -1,12 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     function filterItems() {
-        const searchInput = document.getElementById('search').value.toLowerCase();
+        const searchInput = document.getElementById('search-games').value.toLowerCase();
         document.querySelectorAll('.image-item').forEach(item => {
             const label = item.dataset.label.toLowerCase();
             item.style.display = label.includes(searchInput) ? '' : 'none';
         });
     }
 
-    document.getElementById('search').addEventListener('input', filterItems);
+    document.getElementById('search-games').addEventListener('input', filterItems);
 });

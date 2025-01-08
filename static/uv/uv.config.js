cf1df6a35ed3b56d33f409195f2413cbd75e39f2
petezahv4 <petezahiscool@gmail.com>
2025-01-07 22:23:59 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,4 +1,8 @@
-//  https://v2202412246404304352.megasrv.de/bare/ works too.  Thanks Benrogo :)!!!
+// There are 2 bare servers in PeteZah Games.  
+// The primary is hosted by Benrogo.net and is science.petezahgames.com.  It feeds off of bare.benrogo.net
+// The second is hosted by me and is a backup, it is bare.petezahgames.com.
+// bare-sigma.yourmom.eu.org is one that is blocked and yet works well.
+// https://v2202412246404304352.megasrv.de/bare/ works too.  Thanks Benrogo :)!!!
 
 self.__uv$config = {
     prefix: '/static/ixl/',

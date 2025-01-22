82b27ace6b11f87e8cecb27da9bb08bcc06e0f8d
petezahv4 <petezahiscool@gmail.com>
2025-01-22 18:21:40 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -7,7 +7,7 @@
 
 self.__uv$config = {
     prefix: '/static/ixl/',
-    bare:'https://uhbm.arybarbosa.com/bare/',
+    bare:'https://science.petezahgames.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',

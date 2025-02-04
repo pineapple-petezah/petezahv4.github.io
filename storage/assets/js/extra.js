29b2264cb8b3dc9654f16591796636aa4c89f572
petezahv4 <petezahiscool@gmail.com>
2025-02-04 18:14:48 -0500

    Create extra.js

---
+++ storage/assets/js/extra.js
@@ -0,0 +1,8 @@
+const slides = document.querySelectorAll('.image-slide');
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}, 3000);

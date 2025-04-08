69fc2bfff853a314bffc8de4d86aac5ff2a5e73e
hypackel <contact@hypackel.com>
2025-04-08 18:32:53 -0400

    more formatting ig/starting to improve

--- storage/ag/g/1v1lol/js/IronSourceRV.js
+++ storage/ag/g/1v1lol/js/IronSourceRV.js
@@ -1,33 +1,31 @@
 window.InitRV = function InitRV(id) {
-  $.getScript("https://static.ultra-rv.com/rv-min.js", function () {
+	$.getScript("https://static.ultra-rv.com/rv-min.js", () => {
+		const userId = id === undefined ? "Guest" : id;
+		const ironRV = IronRV.getInstance({
+			applicationKey: "1-mb5whk",
+			applicationUserId: userId,
+		});
 
-    let userId = id === undefined ? "Guest" : id;
-    let ironRV = IronRV.getInstance({
-      applicationKey: "1-mb5whk",
-      applicationUserId: userId,
-    });
+		ironRV.addListener(IronRV.EVENTS.READY, () => {
+			gameInstance.SendMessage("MainMenuManagers", "RvReady");
+		});
 
-    ironRV.addListener(IronRV.EVENTS.READY, function () {
-      gameInstance.SendMessage("MainMenuManagers", "RvReady");
-    });
+		ironRV.addListener(IronRV.EVENTS.CLOSE, () => {
+			gameInstance.SendMessage("MainMenuManagers", "RvWatchComplete", "false");
+		});
 
-    ironRV.addListener(IronRV.EVENTS.CLOSE, function () {
-      gameInstance.SendMessage("MainMenuManagers", "RvWatchComplete", "false");
-    });
+		ironRV.addListener(IronRV.EVENTS.COMPLETION, () => {
+			gameInstance.SendMessage("MainMenuManagers", "RvWatchComplete", "true");
+		});
 
-    ironRV.addListener(IronRV.EVENTS.COMPLETION, function () {
-      gameInstance.SendMessage("MainMenuManagers", "RvWatchComplete", "true");
-    });
+		ironRV.addListener(IronRV.EVENTS.AD_BLOCK, () => {
+			ironRV.showAdBlockMessage();
+		});
 
-    ironRV.addListener(IronRV.EVENTS.AD_BLOCK, function () {
-      ironRV.showAdBlockMessage();
-    });
+		ironRV.addListener(IronRV.EVENTS.INIT_ERROR, () => {});
 
-    ironRV.addListener(IronRV.EVENTS.INIT_ERROR, function () {
-    });
-
-    window.showRV = () => {
-      ironRV.show();
-    };
-  });
-}
+		window.showRV = () => {
+			ironRV.show();
+		};
+	});
+};
--- storage/ag/g/1v1lol/js/windowResize.js
+++ storage/ag/g/1v1lol/js/windowResize.js
@@ -1,40 +1,37 @@
 isIframe = false;
-		if (window.self != window.top) {
-			isIframe = true;
-			function WindowResize() {
-				var v = window.innerWidth;
-				var maxRes = 1320;
+if (window.self != window.top) {
+	isIframe = true;
+	function WindowResize() {
+		var v = window.innerWidth;
+		var maxRes = 1320;
 
-				if (v < maxRes) {
-					var ads = document.getElementsByClassName('ad');
+		if (v < maxRes) {
+			var ads = document.getElementsByClassName("ad");
 
-					for (const ad of ads) {
-						ad.style.transform = "scale(" + v / maxRes + ")";
-					}
-				}
-				else {
-					var ads = document.getElementsByClassName('ad');
-
-					for (const ad of ads) {
-						ad.style.transform = "scale(1)";
-					}
-				}
+			for (const ad of ads) {
+				ad.style.transform = "scale(" + v / maxRes + ")";
 			}
-			window.addEventListener("resize", WindowResize);
-			WindowResize();
+		} else {
+			var ads = document.getElementsByClassName("ad");
+
+			for (const ad of ads) {
+				ad.style.transform = "scale(1)";
+			}
 		}
-		else {
-			var styles = `
+	}
+	window.addEventListener("resize", WindowResize);
+	WindowResize();
+} else {
+	var styles = `
     @media screen and (max-width: 1200px) { 
 		.ad-leaderboard-bottom {
 			display: none !important;
 		}
 	}
-`
+`;
 
-			var styleSheet = document.createElement("style")
-			styleSheet.type = "text/css"
-			styleSheet.innerText = styles
-			document.head.appendChild(styleSheet)
-		}
-		
+	var styleSheet = document.createElement("style");
+	styleSheet.type = "text/css";
+	styleSheet.innerText = styles;
+	document.head.appendChild(styleSheet);
+}

88786bd1205c4df28b6875b75c9b726b8663d43f
pineapple-petezah <petezahindustries@gmail.com>
2025-04-08 21:24:25 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: "/static/petezah/",
-	bare: "https://science.petezahgames.com/",
+	bare: "https://petezahserver.guillaumevanstrydonck.be/bare/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/static/uv/uv.handler.js",
